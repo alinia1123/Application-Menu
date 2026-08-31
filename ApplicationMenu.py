854939d22d4d00bc9235a7c10b5242597813546e
alinia1123 <alinia1123@students.ecpi.edu>
2026-08-30 20:28:34 -0400

    Update ApplicationMenu.py

--- ApplicationMenu.py
+++ ApplicationMenu.py
@@ -1,12 +1,20 @@
 from datetime import datetime
 
-print("alinia1123 Spreadsheet Automation Menu")
-print("1. Input Data")
-print("2. View Current Data")
-print("3. Generate Report")
+menu_options = {
+    "1": "Input Data",
+    "2": "View Current Data",
+    "3": "Generate Report"
+}
 
-# The next line retrieves the inputted option and stores it into the variable called choice.
+print("alinia1123 Spreadsheet Automation Menu")
+
+for number, option in menu_options.items():
+    print(number + ".", option)
+
 choice = input("Enter option: ")
 
-print("You selected option", choice)
-print("The time and date is", str(datetime.now()))
+if choice in menu_options:
+    print("You selected option", choice)
+    print("The time and date is", str(datetime.now()))
+else:
+    print("Error: Invalid choice selected")
